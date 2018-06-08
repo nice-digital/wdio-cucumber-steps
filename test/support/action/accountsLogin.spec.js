0bd34747c1f0a90922a22e71ab2b48eb1580cffb
Warren Keith <warren.keith@nice.org.uk>
2018-06-08 15:48:14 +0100

    Completed test for accountsLogin

--- test/support/action/accountsLogin.spec.js
+++ test/support/action/accountsLogin.spec.js
@@ -1,45 +1,30 @@
 import accountsLogin from "../../../src/support/action/accountsLogin.js";
 import { getNICEAccountsUrl } from "../../../src/support/utils";
+import login from "../../../src/support/action/login";
+jest.mock("../../../src/support/action/login");
 
 describe("accountLogin", () => {
-
 	beforeEach(() => {
 		global.browser = {
 			url: jest.fn(),
 			waitForExist: jest.fn(),
 			setValue: jest.fn(),
-			submitForm: jest.fn()
+			submitForm: jest.fn(),
+			getCookie: jest.fn(() => true)
 		};
 	});
 
-	// it("should not attempt to log in if the auth cookie is present", ()=> {
-
-	// 	global.browser.getCookie = jest.fn(cookieName=>{
-	// 		return cookieName === "__nrpa_2.1";
-	// 	});
-
-	// 	expect(global.browser.url).not.toHaveBeenCalled();
-
-	// });
-
-	it.only("should not call the get Nice accounts URL function if the cookie is not present", ()=> {
-		global.browser.getCookie = jest.fn((arg) => console.log(arg));
+	it("should not call the get Nice accounts URL function if the cookie is not present", ()=> {
+		accountsLogin();
 		expect(global.browser.url).not.toHaveBeenCalled();
+		expect(login).not.toHaveBeenCalled();
 	});
 
 	it("should call the get Nice accounts URL function if the cookie is not present", () => {
-		global.browser.getCookie = jest.fn(() => "null");
+		global.browser.getCookie = jest.fn(() => null);
+		accountsLogin("env", "username", "password");
 		expect(global.browser.url).toHaveBeenCalled();
+		expect(login).toHaveBeenCalledWith("username", "password");
 	});
-
-	// it("should call login function if there's no cookie", ()=>{
-	// 	global.browser.getCookie = jest.fn(() => false);
-	// 	global.browser.url = jest.fn(() => "string");
-
-	// 	expect(accountsLogin.login()).toBeCalled();
-	// 	// 1. browser.url to be called once
-	// 	// 2. login should be called once
-
-	// });
 
 });
