90d64f1974c0d5ec88d7c146062eb1eeb56b807f
Cheguegallagher <martin.gallagher@nice.org.uk>
2019-06-19 15:14:40 +0100

    Increase wait for Accounts to load

--- src/support/action/globalnavLogin.js
+++ src/support/action/globalnavLogin.js
@@ -12,12 +12,12 @@ module.exports = (username, password) => {
 
 	browser.waitForExist("header[aria-label='Site header']");
 	var headerMenuShown = browser.isVisible("#header-menu-button");
-	if (headerMenuShown) {		
+	if (headerMenuShown) {
 		browser.click("#header-menu-button");
 		browser.click("#header-menu a[href*='accounts.nice.org.uk/signin']");
 	} else {
 		browser.click("#header-menu-button+* a[href*='accounts.nice.org.uk/signin']");
-	}	
+	}
 	login(username, password);
 };
 
