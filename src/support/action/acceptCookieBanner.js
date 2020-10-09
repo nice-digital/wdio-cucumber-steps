95fb16aaa6a109ca6ead38a1e9eef27bd63d460c
Liza Adebowale <Liza.Adebowale@nice.org.uk>
2020-10-09 14:54:56 +0100

    made some changes

--- src/support/action/acceptCookieBanner.js
+++ src/support/action/acceptCookieBanner.js
@@ -1,13 +1,18 @@
-// To dismiss cookie banner, as it blocks clicks
+/*! https://github.com/webdriverio/cucumber-boilerplate/blob/master/src/support/action/acceptCookieBanner.js */
+/**
+ * Accept the settings from the cookie banner
+ * @param  {String}   element Element selector
+ */
 
-module.exports = (text) => {
+ // To dismiss cookie banner, as it blocks clicks
 
-	browser.pause(2000);
+ module.exports = (text) => {
+
+	browser.waitForExist('#global-nav-header', 2000)
 
 	const cookieControl = $("body #ccc");
-		if (cookieControl.isVisible("button=Accept all cookies")) {
-		cookieControl.$("button=Accept all cookies").click();
-	}
-		else (browser.refresh())
+	if (cookieControl.isVisible("button.ccc-accept-button")) {
+	cookieControl.$("button.ccc-accept-button").click();
 
-};
+	}
+ };
--- src/when/definitions.js
+++ src/when/definitions.js
@@ -1,3 +1,4 @@
+import acceptCookieBanner from "../support/action/acceptCookieBanner"
 import clearInputField from "../support/action/clearInputField";
 import clickElement from "../support/action/clickElement";
 import closeLastOpenedWindow from "../support/action/closeLastOpenedWindow";
@@ -87,6 +88,9 @@ export const stepDefinitions = [
 
 	When(/^I focus on the element "([^"]+)"$/, focusElement).withDocs(
 		"Move focus to the given element"),
+
+	When(/^I accept the cookie settings$/, acceptCookieBanner)
+		.withDocs("Accept the cookie banner settings"),
 ];
 
 export default stepDefinitions;
