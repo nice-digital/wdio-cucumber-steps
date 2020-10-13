e970261fb88a871e3a605937387625934f29e0ac
Liza Adebowale <Liza.Adebowale@nice.org.uk>
2020-10-13 13:44:13 +0100

    script refactored and unit test added

--- src/support/action/acceptCookieBanner.js
+++ src/support/action/acceptCookieBanner.js
@@ -2,12 +2,12 @@
  * Accept all cookies using the NICE cookie banner.
  * We use this as the cookie banner blocks clicks.
  */
- module.exports = () => {
+module.exports = () => {
+		browser.waitForExist("body #ccc", 2000);
+		const isVisible = browser.isVisible("button.ccc-accept-button");
+		if (isVisible) {
+			browser.click("button.ccc-accept-button");
+		}
+	 };
 
-	browser.waitForExist("body #ccc", 2000)
 
-	const cookieControl = $("body #ccc");
-	if (cookieControl.isVisible("button.ccc-accept-button")) {
-		cookieControl.$("button.ccc-accept-button").click();
-	}
- };
