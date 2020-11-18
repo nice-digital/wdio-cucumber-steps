48d3f1a585fc852f8b98c431488fff24ed914bf7
Cheguegallagher <martin.gallagher@nice.org.uk>
2020-11-18 09:59:30 +0000

    Converted all checks files to TS

--- src/support/utils.js
+++ src/support/utils.js
@@ -8,7 +8,7 @@ type StepDefType = {
 	examples: string[],
 	withDocs: (string) => StepDefType,
 	withExample: (string) => StepDefType,
-  };
+};
 
 const cucumberFunction = (pattern: string | RegExp, code): StepDefType => ({
 	pattern: pattern,
@@ -16,18 +16,18 @@ const cucumberFunction = (pattern: string | RegExp, code): StepDefType => ({
 	code: code,
 	examples: [],
 	title: "",
-	withOptions: function(options) {
+	withOptions: function (options) {
 		this.options = options;
 		return this;
 	},
-	withDocs: function(title: string): StepDefType {
+	withDocs: function (title: string): StepDefType {
 		this.title = title;
 		return this;
 	},
-	withExample: function(example: string): StepDefType {
+	withExample: function (example: string): StepDefType {
 		this.examples.push(example);
 		return this;
-	}
+	},
 });
 
 const Given: StepDefType = cucumberFunction;
@@ -40,8 +40,8 @@ const Then: StepDefType = cucumberFunction;
  * @param {Array<StepDefType>} steps The array of step objects
  */
 const applyStepDefinitions = (fn, steps) => {
-	steps.forEach(step => {
-		if(step.options) {
+	steps.forEach((step) => {
+		if (step.options) {
 			fn(step.pattern, step.options, step.code);
 		} else {
 			fn(step.pattern, step.code);
@@ -55,20 +55,15 @@ const applyStepDefinitions = (fn, steps) => {
  */
 const getNICEAccountsUrl = (environment: string): string | false => {
 	function isValidEnvironment(env) {
-		return (env === "live") || (env === "beta") || (env === "test");
+		return env === "live" || env === "beta" || env === "test";
 	}
 
 	if (isValidEnvironment(environment)) {
-		const accountsHostPrefix: string = environment === "live" ? "" : `${environment}-`;
+		const accountsHostPrefix: string =
+			environment === "live" ? "" : `${environment}-`;
 		return `https://${accountsHostPrefix}accounts.nice.org.uk`;
 	}
 	return false;
 };
 
-export {
-	Given,
-	When,
-	Then,
-	applyStepDefinitions,
-	getNICEAccountsUrl
-};
+export { Given, When, Then, applyStepDefinitions, getNICEAccountsUrl };
