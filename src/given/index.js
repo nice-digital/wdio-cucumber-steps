5aefaec1d453b65ceaaeb511a162d213c7a2a1ab
Ian Routledge <ian.routledge@nice.org.uk>
2020-12-07 20:46:14 +0000

    Refactor definition files
    Remove list of definitions

--- src/given/index.js
+++
@@ -1,7 +0,0 @@
-import { Given } from "cucumber";
-
-import { applyStepDefinitions } from "../support/utils";
-
-import stepDefinitions from "./definitions";
-
-applyStepDefinitions(Given, stepDefinitions);
