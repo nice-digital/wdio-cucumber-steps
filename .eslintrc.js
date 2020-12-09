a834d7276fc2d572a9dace11e7c8c135f87f7330
Ian Routledge <ian.routledge@nice.org.uk>
2020-12-09 19:46:21 +0000

    Fix TypeScript lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,56 @@
 module.exports = {
 	root: true,
-	parser: "@typescript-eslint/parser",
-	extends: [
-		"eslint:recommended",
-		"plugin:prettier/recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended",
-	],
 	plugins: ["@typescript-eslint", "prettier"],
-	extends: [
-		"plugin:prettier/recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended",
-	],
+	extends: ["eslint:recommended", "plugin:prettier/recommended"],
+	env: {
+		node: true,
+	},
+	globals: {
+		$: "readonly",
+		$$: "readonly",
+		browser: "readonly",
+	},
 	parserOptions: {
-		ecmaVersion: 2018,
+		ecmaVersion: 2019,
 		sourceType: "module",
 	},
-	rules: {
-		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-		// e.g. "@typescript-eslint/explicit-function-return-type": "off",
-		"@typescript-eslint/no-explicit-any": 2,
-	},
 	overrides: [
+		{
+			files: ["./test/**/*.js"],
+			env: {
+				jest: true,
+			},
+		},
 		{
 			files: ["*.js"],
 			rules: {
 				"@typescript-eslint/explicit-function-return-type": "off",
 				"@typescript-eslint/no-var-requires": "off",
 				"@typescript-eslint/explicit-module-boundary-types": "off",
+			},
+		},
+		{
+			files: [`**/*.ts`],
+			parser: `@typescript-eslint/parser`,
+			parserOptions: {
+				project: [
+					`./tsconfig.json`,
+					`./src/tsconfig.json`,
+					`./test/tsconfig.json`,
+				],
+			},
+			extends: [
+				"eslint:recommended",
+				"plugin:prettier/recommended",
+				"plugin:@typescript-eslint/eslint-recommended",
+				"plugin:@typescript-eslint/recommended",
+				`prettier/@typescript-eslint`,
+			],
+			rules: {
+				// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+				// e.g. "@typescript-eslint/explicit-function-return-type": "off",
+				"@typescript-eslint/no-explicit-any": 2,
+				"@typescript-eslint/no-misused-promises": 1,
 			},
 		},
 	],
